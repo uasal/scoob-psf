060b195dbfb8b37e1b8c319b3675d147fc4109f6
Kian Milani <kianmilani@email.arizona.edu>
2023-10-20 16:48:16 -0700

    working on differentiating jax model but my GRADIENT IS ALL ZEROS!

--- scoobpsf/jax_scoob.py
+++ scoobpsf/jax_scoob.py
@@ -23,15 +23,6 @@ device = jax.devices()[0].device_kind
 print(f'Jax platform: {platform}')
 print(f'Jax device: {device}')
 
-# MISCELLANEOUS FUNCTIONS
-# def ensure_np_array(arr):
-#     if isinstance(arr, np.ndarray):
-#         return arr
-#     elif isinstance(arr, jax.numpy.ndarray):
-#         return np.asarray(arr)
-#     elif isinstance(arr, cp.ndarray):
-#         return arr.get()
-
 def pad_or_crop( arr_in, npix ):
     n_arr_in = arr_in.shape[0]
     if n_arr_in == npix:
@@ -46,6 +37,32 @@ def pad_or_crop( arr_in, npix ):
         x2 = x1 + n_arr_in
         arr_out = arr_out.at[x1:x2,x1:x2].set(arr_in)
     return arr_out
+
+def interp_arr(arr, pixelscale, new_pixelscale, order=1):
+        Nold = arr.shape[0]
+        old_xmax = pixelscale * Nold/2
+
+        x,y = jnp.ogrid[-old_xmax:old_xmax-pixelscale:Nold*1j,
+                       -old_xmax:old_xmax-pixelscale:Nold*1j]
+
+        Nnew = int(np.ceil(2*old_xmax/new_pixelscale)) - 1
+        new_xmax = new_pixelscale * Nnew/2
+
+        newx,newy = jnp.mgrid[-new_xmax:new_xmax-new_pixelscale:Nnew*1j,
+                              -new_xmax:new_xmax-new_pixelscale:Nnew*1j]
+
+        x0 = x[0,0]
+        y0 = y[0,0]
+        dx = x[1,0] - x0
+        dy = y[0,1] - y0
+
+        ivals = (newx - x0)/dx
+        jvals = (newy - y0)/dy
+
+        coords = jnp.array([ivals, jvals])
+
+        interped_arr = jax.scipy.ndimage.map_coordinates(arr, coords, order=order)
+        return interped_arr
 
 # MAKE OPTICAL ELEMENT FUNCTIONS
 
@@ -81,6 +98,7 @@ def make_focal_grid(npix, oversample, polar=True):
     pxscl = 1/oversample
     
     xx = (jnp.linspace(0, N-1, N) - N/2 + 1/2)*pxscl
+    print(xx)
     x,y = jnp.meshgrid(xx,xx)
 
     if polar:
@@ -129,9 +147,7 @@ def generate_wfe(diam,
     wfe_opd = jnp.asarray(ensure_np_array(wfe_opd))
 
     mask = ensure_np_array(poppy.CircularAperture(radius=diam/2).get_transmission(wf))>0
-    # imshow1(mask)
     Zs = ensure_np_array(poppy.zernike.arbitrary_basis(mask, nterms=3, outside=0))
-    # imshow3(Zs[0], Zs[1], Zs[2])
     
     Zc_amp = lstsq(Zs, wfe_amp)
     Zc_opd = lstsq(Zs, wfe_opd)
@@ -162,92 +178,103 @@ def ifft(arr):
     iftarr = jnp.fft.ifftshift(jnp.fft.ifft2(jnp.fft.fftshift(arr)))
     return iftarr
 
-def mft(wavefront, nlamD, npix, forward=True, centering='ADJUSTABLE'):
-        '''
-        npix : int
-            Number of pixels per side side of destination plane array (corresponds
-            to 'N_B' in Soummer et al. 2007 4.2). This will be the # of pixels in
-            the image plane for a forward transformation, in the pupil plane for an
-            inverse.
-        '''
-        
-        # this code was duplicated from POPPY's MFT method
-        npupY, npupX = wavefront.shape
-        nlamDX, nlamDY = nlamD, nlamD
-        npixY, npixX = npix, npix
-        
-        if forward:
-            dU = nlamDX / float(npixX)
-            dV = nlamDY / float(npixY)
-            dX = 1.0 / float(npupX)
-            dY = 1.0 / float(npupY)
-        else:
-            dX = nlamDX / float(npupX)
-            dY = nlamDY / float(npupY)
-            dU = 1.0 / float(npixX)
-            dV = 1.0 / float(npixY)
-        
-        if centering=='ADJUSTABLE':
-            offsetY, offsetX = 0.0, 0.0
-            Xs = (jnp.arange(npupX, dtype=float) - float(npupX) / 2.0 - offsetX + 0.5) * dX
-            Ys = (jnp.arange(npupY, dtype=float) - float(npupY) / 2.0 - offsetY + 0.5) * dY
+def mft(wavefront, nlamD, npix,):
+    # this code was duplicated from POPPY's MFT method
+    npupY, npupX = wavefront.shape
+    nlamDX, nlamDY = nlamD, nlamD
+    npixY, npixX = npix, npix
 
-            Us = (jnp.arange(npixX, dtype=float) - float(npixX) / 2.0 - offsetX + 0.5) * dU
-            Vs = (jnp.arange(npixY, dtype=float) - float(npixY) / 2.0 - offsetY + 0.5) * dV
-        elif centering=='FFTSTYLE':
-            Xs = (jnp.arange(npupX, dtype=float) - (npupX / 2)) * dX
-            Ys = (jnp.arange(npupY, dtype=float) - (npupY / 2)) * dY
+    dU = nlamDX / float(npixX)
+    dV = nlamDY / float(npixY)
+    dX = 1.0 / float(npupX)
+    dY = 1.0 / float(npupY)
 
-            Us = (jnp.arange(npixX, dtype=float) - npixX / 2) * dU
-            Vs = (jnp.arange(npixY, dtype=float) - npixY / 2) * dV
-        
-        XU = jnp.outer(Xs, Us)
-        YV = jnp.outer(Ys, Vs)
-        
-        if forward:
-            expXU = jnp.exp(-2.0 * np.pi * -1j * XU)
-            expYV = jnp.exp(-2.0 * np.pi * -1j * YV).T
-            t1 = jnp.dot(expYV, wavefront)
-            t2 = jnp.dot(t1, expXU)
-        else:
-            expYV = jnp.exp(-2.0 * np.pi * 1j * YV).T
-            expXU = jnp.exp(-2.0 * np.pi * 1j * XU)
-            t1 = jnp.dot(expYV, wavefront)
-            t2 = jnp.dot(t1, expXU)
+    offsetY, offsetX = 0.0, 0.0
+    Xs = (jnp.arange(npupX, dtype=float) - float(npupX) / 2.0 - offsetX + 0.5) * dX
+    Ys = (jnp.arange(npupY, dtype=float) - float(npupY) / 2.0 - offsetY + 0.5) * dY
 
-        norm_coeff = np.sqrt((nlamDY * nlamDX) / (npupY * npupX * npixY * npixX))
+    Us = (jnp.arange(npixX, dtype=float) - float(npixX) / 2.0 - offsetX + 0.5) * dU
+    Vs = (jnp.arange(npixY, dtype=float) - float(npixY) / 2.0 - offsetY + 0.5) * dV
+
+    XU = jnp.outer(Xs, Us)
+    YV = jnp.outer(Ys, Vs)
+
+    expXU = jnp.exp(-2.0 * np.pi * -1j * XU)
+    expYV = jnp.exp(-2.0 * np.pi * -1j * YV).T
+    t1 = jnp.dot(expYV, wavefront)
+    t2 = jnp.dot(t1, expXU)
+
+    norm_coeff = np.sqrt((nlamDY * nlamDX) / (npupY * npupX * npixY * npixX))
+
+    return norm_coeff * t2
+
+# def mft(wavefront, nlamD, npix, forward=True, centering='ADJUSTABLE'):
+#         '''
+#         npix : int
+#             Number of pixels per side side of destination plane array (corresponds
+#             to 'N_B' in Soummer et al. 2007 4.2). This will be the # of pixels in
+#             the image plane for a forward transformation, in the pupil plane for an
+#             inverse.
+#         '''
         
-        return norm_coeff * t2
+#         # this code was duplicated from POPPY's MFT method
+#         npupY, npupX = wavefront.shape
+#         nlamDX, nlamDY = nlamD, nlamD
+#         npixY, npixX = npix, npix
+        
+#         if forward:
+#             dU = nlamDX / float(npixX)
+#             dV = nlamDY / float(npixY)
+#             dX = 1.0 / float(npupX)
+#             dY = 1.0 / float(npupY)
+#         else:
+#             dX = nlamDX / float(npupX)
+#             dY = nlamDY / float(npupY)
+#             dU = 1.0 / float(npixX)
+#             dV = 1.0 / float(npixY)
+        
+#         if centering=='ADJUSTABLE':
+#             offsetY, offsetX = 0.0, 0.0
+#             Xs = (jnp.arange(npupX, dtype=float) - float(npupX) / 2.0 - offsetX + 0.5) * dX
+#             Ys = (jnp.arange(npupY, dtype=float) - float(npupY) / 2.0 - offsetY + 0.5) * dY
+
+#             Us = (jnp.arange(npixX, dtype=float) - float(npixX) / 2.0 - offsetX + 0.5) * dU
+#             Vs = (jnp.arange(npixY, dtype=float) - float(npixY) / 2.0 - offsetY + 0.5) * dV
+#         elif centering=='FFTSTYLE':
+#             Xs = (jnp.arange(npupX, dtype=float) - (npupX / 2)) * dX
+#             Ys = (jnp.arange(npupY, dtype=float) - (npupY / 2)) * dY
+
+#             Us = (jnp.arange(npixX, dtype=float) - npixX / 2) * dU
+#             Vs = (jnp.arange(npixY, dtype=float) - npixY / 2) * dV
+        
+#         XU = jnp.outer(Xs, Us)
+#         YV = jnp.outer(Ys, Vs)
+        
+#         if forward:
+#             expXU = jnp.exp(-2.0 * np.pi * -1j * XU)
+#             expYV = jnp.exp(-2.0 * np.pi * -1j * YV).T
+#             t1 = jnp.dot(expYV, wavefront)
+#             t2 = jnp.dot(t1, expXU)
+#         else:
+#             expYV = jnp.exp(-2.0 * np.pi * 1j * YV).T
+#             expXU = jnp.exp(-2.0 * np.pi * 1j * XU)
+#             t1 = jnp.dot(expYV, wavefront)
+#             t2 = jnp.dot(t1, expXU)
+
+#         norm_coeff = np.sqrt((nlamDY * nlamDX) / (npupY * npupX * npixY * npixX))
+        
+#         return norm_coeff * t2
     
     
-# DM Functions    
+# Forward Model
 
-def apply_dm(wavefront, ):
-
-    dm_surf = get_dm_surface()
-    if include_reflection:
-        dm_surf *= 2
-    dm_surf = pad_or_crop(dm_surf, N)
-    wavefront *= jnp.exp(1j*2*np.pi/wavelength.to_value(u.m) * dm_surf)
-    return wavefront
-    
-
-def forward_model(wavefront, WFE, FPM, LYOT,
-                  actuators, inf_matrix, inf_pixelscale,
+def forward_model(wavefront, WFE, dm_phasor, FPM, LYOT, 
                   npix, oversample, 
-                  pupil_diam=6.75*u.mm, npsf=200, pixelscale_lamD=1/5, 
-                  dm_fill_factor=0.94, dm_active_diam=10.2*u.mm,
+                  npsf=200, pixelscale_lamD=1/4, 
                   Imax_ref=1):
 
         N = int(npix*oversample)
-        pupil_pxscl = pupil_diam/(npix*u.pix)
-        # imshow1(wavefront, npix=npix)
         wavefront *= WFE # apply WFE data
-
-        dm_pixelscale = dm_fill_factor * dm_active_diam/(npix*u.pix)
-        dm_phasor = jax_dm.get_phasor(actuators, inf_matrix, inf_pixelscale=inf_pixelscale, pixelscale=dm_pixelscale)
-        dm_phasor = pad_or_crop(dm_phasor, N)
-        # imshow1(jnp.angle(dm_phasor), npix=npix)
         wavefront *= dm_phasor
         
         wavefront = fft(wavefront)
@@ -264,19 +291,22 @@ def forward_model(wavefront, WFE, FPM, LYOT,
         
         return wavefront
 
-    
-def snap(wavefront, WFE, FPM, LYOT,
-        actuators, inf_matrix, inf_pixelscale,
+def get_dm_phasor(actuators, inf_matrix, inf_pixelscale, npix,
+                  dm_active_diam=0.0102, dm_fill_factor=0.94,
+                  wavelength=650e-9):
+    surf = inf_matrix.dot(actuators).reshape(int(jnp.sqrt(inf_matrix.shape[0])), int(jnp.sqrt(inf_matrix.shape[0])))
+    opd = 2*interp_arr(surf, inf_pixelscale, dm_fill_factor * dm_active_diam/npix)
+    phasor = jnp.exp(1j*2*jnp.pi/wavelength * opd)
+    return phasor
+
+def snap(wavefront, WFE, dm_phasor, FPM, LYOT,
         npix, oversample, 
-        pupil_diam=6.75*u.mm, npsf=200, pixelscale_lamD=1/5, 
-        dm_fill_factor=0.94, dm_active_diam=10.2*u.mm,
+        npsf=200, pixelscale_lamD=1/5, 
         Imax_ref=1,
          plot=False, vmax=None, vmin=None, grid=False):
-        fpwf = forward_model(wavefront, WFE, FPM, LYOT,
-                             actuators, inf_matrix, inf_pixelscale,
+        fpwf = forward_model(wavefront, WFE, dm_phasor, FPM, LYOT,
                              npix, oversample, 
-                             pupil_diam=pupil_diam, npsf=npsf, pixelscale_lamD=pixelscale_lamD, 
-                             dm_fill_factor=dm_fill_factor, dm_active_diam=dm_active_diam,
+                             npsf=npsf, pixelscale_lamD=pixelscale_lamD, 
                              Imax_ref=Imax_ref)
         image = jnp.abs(fpwf)**2
         if plot:
