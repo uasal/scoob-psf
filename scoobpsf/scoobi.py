3c95a3556bf681e08714bb29c0ddd3b4280fe249
Kian Milani <kianmilani@email.arizona.edu>
2024-07-26 11:51:00 -0700

    imporving the tesbed interface and taking gain into account for normalization

--- scoobpsf/scoobi.py
+++ scoobpsf/scoobi.py
@@ -8,7 +8,6 @@ import os
 from pathlib import Path
 
 from .math_module import xp, _scipy, ensure_np_array
-from . import imshows
 from .utils import pad_or_crop
 import scoobpsf
 
@@ -16,73 +15,53 @@ module_path = Path(os.path.dirname(os.path.abspath(scoobpsf.__file__)))
 
 import poppy
 
-try:
-    import scoobpy
-    from scoobpy import utils as scoob_utils
-    from magpyx.utils import ImageStream
+from scoobpy import utils as scoob_utils
+import purepyindi
+import purepyindi2
+from magpyx.utils import ImageStream
 
-    import purepyindi
-    from purepyindi import INDIClient
-    client0 = INDIClient('localhost', 7624)
-    client0.start()
+def move_psf(x_pos, y_pos, client):
+    client.wait_for_properties(['stagepiezo.stagepupil_x_pos', 'stagepiezo.stagepupil_y_pos'])
+    scoob_utils.move_relative(client, 'stagepiezo.stagepupil_x_pos', x_pos)
+    time.sleep(0.25)
+    scoob_utils.move_relative(client, 'stagepiezo.stagepupil_y_pos', y_pos)
+    time.sleep(0.25)
 
-    import purepyindi2
-    from purepyindi2 import IndiClient
-    client = IndiClient()
-    client.connect()
-    client.get_properties()
-
-    scoobpy_avail = True
-    print('Succesfully initialized testbed interface.')
-except ImportError:
-    print('Could not import scoobpy. Testbed interface unavailable.')
-    scoobpy_avail = False
-
-def move_psf(x_pos, y_pos):
-        client0.wait_for_properties(['stagepiezo.stagepupil_x_pos', 'stagepiezo.stagepupil_y_pos'])
-        scoob_utils.move_relative(client0, 'stagepiezo.stagepupil_x_pos', x_pos)
-        time.sleep(0.25)
-        scoob_utils.move_relative(client0, 'stagepiezo.stagepupil_y_pos', y_pos)
-        time.sleep(0.25)
-
-def set_zwo_roi(xc, yc, npix, delay=0.25):
+def set_zwo_roi(xc, yc, npix, client, delay=0.25):
     # update roi parameters
-    client0.wait_for_properties(['scicam.roi_region_x', 'scicam.roi_region_y', 'scicam.roi_region_h' ,'scicam.roi_region_w', 'scicam.roi_set'])
-    client0['scicam.roi_region_x.target'] = xc
-    client0['scicam.roi_region_y.target'] = yc
-    client0['scicam.roi_region_h.target'] = npix
-    client0['scicam.roi_region_w.target'] = npix
+    client.wait_for_properties(['scicam.roi_region_x', 'scicam.roi_region_y', 'scicam.roi_region_h' ,'scicam.roi_region_w', 'scicam.roi_set'])
+    client['scicam.roi_region_x.target'] = xc
+    client['scicam.roi_region_y.target'] = yc
+    client['scicam.roi_region_h.target'] = npix
+    client['scicam.roi_region_w.target'] = npix
     time.sleep(delay)
-    client0['scicam.roi_set.request'] = purepyindi.SwitchState.ON
-    time.sleep(delay)
-
-def set_zwo_exp_time(exp_time, delay=0.1):
-    client0.wait_for_properties(['scicam.exptime'])
-    client0['scicam.exptime.target'] = exp_time
+    client['scicam.roi_set.request'] = purepyindi.SwitchState.ON
     time.sleep(delay)
 
-def get_zwo_exp_time():
-    client0.wait_for_properties(['scicam.exptime'])
-    return client0['scicam.exptime.current']
+def set_zwo_exp_time(exp_time, client, delay=0.1):
+    client.wait_for_properties(['scicam.exptime'])
+    client['scicam.exptime.target'] = exp_time
+    time.sleep(delay)
 
-def set_fib_atten(value, delay=0.1):
+def get_zwo_exp_time(client):
+    client.wait_for_properties(['scicam.exptime'])
+    return client['scicam.exptime.current']
+
+def set_zwo_emgain(gain, client, delay=0.1):
+    client.wait_for_properties(['scicam.emgain'])
+    client['scicam.emgain.target'] = gain
+    time.sleep(delay)
+
+def get_zwo_emgain(client):
+    client.wait_for_properties(['scicam.emgain'])
+    return client['scicam.emgain.current']
+
+def set_fib_atten(value, client, delay=0.1):
     client['fiberatten.atten.target'] = value
     time.sleep(delay)
 
-def get_fib_atten():
-    return client['fiberatten.atten.target']
-
-
-def move_stage(value):
-    client['stag']
-
-def move_pol(rel_pos): # this will change the throughput by rotating the polarizer
-    # FIXME
-    utils.move_relative(client, 'stagepiezo.stagepol', rel_pos)
-    
-def move_fold(pos):
-    # FIXME
-    client['stagepiezo.stagefold_pos.current'] = pos
+def get_fib_atten(client):
+    return client['fiberatten.atten.current']
 
 # define more functions for moving the fold mirror, using the tip tilt mirror, and the polarizers
 
@@ -142,28 +121,57 @@ class SCOOBI():
 
         # Init camera settings
         self.psf_pixelscale = 3.76e-6*u.m/u.pix
-        self.psf_pixelscale_lamD = 0.17
+        self.psf_pixelscale_lamD = 0.307
         self.Nframes = Nframes
         
         self.npsf = npsf
         self.nbits = 16
 
-        self.blacklevel = 63
-        self.gain = gain
-        self.exp_time = exp_time
-        self.attenuation = attenuation
+        self.texp = 1
+        self.att = 1
+        self.gain = 1
 
-        self.subtract_bias = False
+        self.df = None
+        self.subtract_dark = False
+
         self.return_ni = False
 
         self.Imax_ref = 1
-        self.att_ref = 1
         self.texp_ref = 1
+        self.att_ref = 1
         self.gain_ref = 1
         
         self.x_shift = x_shift
         self.y_shift = y_shift
 
+    def set_zwo_exp_time(self, exp_time, client, delay=0.25):
+        client.wait_for_properties(['scicam.exptime'])
+        client['scicam.exptime.target'] = exp_time
+        time.sleep(delay)
+        self.texp = client['scicam.exptime.current']
+        print(f'Set the ZWO exposure time to {self.texp:.2e}s')
+
+    def set_fib_atten(self, value, client, delay=0.1):
+        client['fiberatten.atten.target'] = value
+        time.sleep(delay)
+        self.att = value
+        print(f'Set the fiber attenuation to {value:.1f}')
+
+    def set_zwo_emgain(self, gain, client, delay=0.1):
+        client.wait_for_properties(['scicam.emgain'])
+        client['scicam.emgain.target'] = gain
+        time.sleep(delay)
+        self.gain = gain
+        print(f'Set the ZWO gain setting to {gain:.1f}')
+
+    def take_dark(self, client, delay=0.5):
+        client.wait_for_properties(['fiberatten.atten'])
+        client['stagelinear.presetName.block_in'] = purepyindi.SwitchState.ON
+        time.sleep(delay)
+        self.df = self.snap()
+        client.wait_for_properties(['fiberatten.atten'])
+        client['stagelinear.presetName.block_out'] = purepyindi.SwitchState.ON
+        time.sleep(delay)
 
     def zero_dm(self):
         self.DM.write(np.zeros(self.dm_shape))
@@ -188,14 +196,15 @@ class SCOOBI():
         else:
             return xp.array(self.DM.grab_latest())/1e6
     
-    def show_dm(self):
-        imshows.imshow2(self.get_dm(), self.get_dm(total=True), self.dm_channel, 'dm00disp')
-    
     def close_dm(self):
         self.DM.close()
 
     def normalize(self, image):
-        image_ni = image/self.Imax_ref * (self.texp_ref/self.texp) * 10**((self.att-self.att_ref)/10)  # * (self.gain_ref/self.gain)
+        image_ni = image/self.Imax_ref
+        image_ni *= (self.texp_ref/self.texp)
+        image_ni *= 10**((self.att-self.att_ref)/10)
+        image_ni *= 10**(-self.gain/20 * 0.1) / 10**(-self.gain_ref/20 * 0.1)
+        # gain ~ 10^(-gain_setting/20 * 0.1)
         return image_ni
 
     def snap(self, normalize=False, plot=False, vmin=None):
@@ -209,8 +218,8 @@ class SCOOBI():
         im = _scipy.ndimage.shift(im, (self.y_shift, self.x_shift), order=0)
         im = pad_or_crop(im, self.npsf)
 
-        if self.subtract_bias:
-            im -= self.bias
+        if self.subtract_dark and self.df is not None:
+            im -= self.df
             
         if self.return_ni:
             im = self.normalize(im)
